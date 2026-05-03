c7ea779c846e6a686b55473b9d9d0715b76b5c86
daniellaeme <daniella.e.eneobong@gmail.com>
2026-05-03 16:12:22 +0100

    Updated features.py for SHAP analysis and to prevent Deprecation warnings using a modern Fingerprint Generator.

--- src/features.py
+++ src/features.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 from rdkit import Chem, DataStructs
-from rdkit.Chem import AllChem
+from rdkit.Chem import Draw, rdFingerprintGenerator
 
-
-def smiles_to_morgan(smiles, radius=2, n_bits=2048, return_as_numpy=True):
+mfpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
+def smiles_to_morgan(smiles, return_as_numpy=True):
     """
     Converts the SMILES string into a Mogan fingerprint.
     If return_as_numpy is True, returns a numpy array (for ML model).
@@ -13,20 +13,17 @@ def smiles_to_morgan(smiles, radius=2, n_bits=2048, return_as_numpy=True):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         return None
-    fingerprint = AllChem.GetMorganFingerprintAsBitVect(mol, radius, n_bits)
 
-    if not return_as_numpy:
-        return fingerprint
-
-    fingerprint_array = np.zeros((0,), dtype=int)
-    DataStructs.ConvertToNumpyArray(fingerprint, fingerprint_array)
-
-    return fingerprint_array
+    if return_as_numpy:
+        return mfpgen.GetFingerprintAsNumpy(mol)
+    else:
+        return mfpgen.GetFingerprint(mol)
 
 
 def get_tanimoto_matrix(fingerprint_list):
     """
     Takes a list of fingerprints and returns an NxN similarity matrix.
+    The fingerprints passed here must have been generated with return_as_numpy=False.
     """
     n = len(fingerprint_list)
     similarity_matrix = []
@@ -36,3 +33,30 @@ def get_tanimoto_matrix(fingerprint_list):
         similarity_matrix.append(similarities)
 
     return np.array(similarity_matrix)
+
+
+def visualise_morgan_bit(smiles_list, bit_id, radius=2, n_bits=2048):
+    """
+    Searches a list of SMILES for a molecule that activates a specific Morgan bit.
+    Returns an image highlighting the substructure.
+    For SHAP analysis.
+    """
+    local_gen = rdFingerprintGenerator.GetMorganGenerator(radius=radius, fpSize=n_bits)
+    for smiles in smiles_list:
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            continue
+        info = {}
+        # Get the fingerprint and populate the info dictionary with bit mappings
+        _ = local_gen.GetFingerprint(mol, additionalOutput=info)
+
+        # Check if our target SHAP bit was activated in this specific molecule
+        if bit_id in info:
+            print(f"Found Bit {bit_id} in SMILES: {smiles}")
+            # Draw the molecule, highlighting the atoms responsible for the bit
+            img = Draw.DrawMorganBit(mol, bit_id, info)
+            return img
+
+    print(f"Could not find a molecule with Bit {bit_id} in the provided list.")
+    return None
+    
